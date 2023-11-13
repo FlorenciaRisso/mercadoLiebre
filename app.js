c6fa7a9056736c192c7af4ec40f840d10a52d001
Florencia Risso <florencia.risso206@gmail.com>
2023-11-13 19:46:47 -0300

    comparto proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,28 @@
+//require express
+const express=require('express');
+
+//Ejecucion de express
+const app=express();
+
+//Require path
+const path=require('path');
+
+//Usando recursos estaticos
+app.use(express.static('public'));
+
+//Levantando el servidor Puerto 3000
+app.listen(3000,()=>console.log("Exito")); 
+
+//RUTAS
+
+app.get('/', (req,res) =>{ 
+    res.sendFile(path.resolve(__dirname,'./views/home.html')); 
+});
+
+app.get('/register.html',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.get('/login.html',(req,res) =>{
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+});
